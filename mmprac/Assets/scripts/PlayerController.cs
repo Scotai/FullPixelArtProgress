7ab96f5953fba611de21dd04446fea5f34db9c79
Lloyd Hussell <lloydhussell@gmail.com>
2019-04-26 14:18:20 +0800

    first commit
    This is what I have been able to work with so far. Pixels are perfect. movement is decent for now(can be changed). Jumping is total trash.... needs fixing

---
+++ mmprac/Assets/scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 3.0f;
+    public float jumpForce = 4.0f;
+    public Rigidbody2D rb;
+    public Vector2 movement;
+    public bool isJumping;
+    public Animator animate;
+    public Vector2 jumping = new Vector2(0,1);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        //animate = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //animate.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+
+    }
+    void FixedUpdate()
+    {
+
+        moveCharacter(movement);
+        jumpCharacter();
+        
+
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.velocity = direction * speed;
+
+    }
+
+    void jumpCharacter()
+    {
+        if(Input.GetKey(KeyCode.Space) && !isJumping)
+        {
+            isJumping = true;
+
+            rb.velocity = jumping * jumpForce;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+}
